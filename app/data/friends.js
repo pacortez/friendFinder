a5003fcf88019c1a4057881cf0323368486e68e2
Paulina Cortez <paulina.a.cortez@gmail.com>
2019-06-15 08:22:03 -0700

    total diff logic added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,76 @@
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+
+// ```json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+// ```
+
+
 var friendsArray = [
     {
-        name: "",
-        photo: "",
-        scores: []
-    }
-]
+        name: "Pau",
+        photo: "image",
+        scores: [
+            1, 
+            2, 
+            3, 
+            4, 
+            5, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5],
+        totalScore: 30
+    },
+    {
+        name: "twos",
+        photo: "image",
+        scores: [
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2],
+    },
+    {
+        name: "threes",
+        photo: "image",
+        scores: [
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3],
+    },
+];
+
+
+module.exports = friendsArray;
+
+
+    
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,16 +4,49 @@
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will 
 // also be used to handle the compatibility logic.
 
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
+
+    //req.body is a form.  When we go to a page with a form, that is how we capture all the input into that form. 
     app.post("/api/friends", function(req, res) {
+
         friends.push(req.body);
 
-        
-        //need to add compatibility logic here?????
+        currentFriendScore = 0;
+        currentFriendArray = req.body.scores.map(Number);
+        currentFriendScore = currentFriendArray.reduce(myFunc);
+            function myFunc(total, num) {
+                return (total + num);
+            }
+        console.log("current F Name: " + req.body.name + "\ncurrent FScore " + currentFriendScore + "\n====")
+
+        for (var i = 0; i < friends.length-1; i++) {
+            // console.log('friends ',friends[i]);
+            //this is taking the score array and turning it into numbers
+            var friendScoreArray = friends[i].scores.map(Number);
+            //this reduce function is adding all the numbers to give a total score
+            friendScore = friendScoreArray.reduce(myFunc);
+                function myFunc(total, num) {
+                    return (total + num);
+                }
+            console.log("friend name: " + friends[i].name + "\nfriend score" + friendScore);
+
+            absoulteDiff = Math.abs(friendScore - currentFriendScore)
+            console.log("absolute Diff" + absoulteDiff + "\n========");
+        };
+
+
     });
+
+
 };
+
+
+
+
+
+
